b236ef8c064a0c9fcef614d20a4cd4150b5abde2
h8art <artem.gershteyn@gmail.com>
2019-06-29 16:28:16 +0300

    upd

--- main.go
+++ main.go
@@ -2,26 +2,21 @@ package main
 
 import "fmt"
 
-func findMax(s []interface{}) interface{} {
+func findMax(s []interface{}, f func(interface{}, interface{}) interface{}) interface{} {
 	max := s[0]
-	switch s[0].(type) {
-	case int:
-		for _, v := range s {
-			if max.(int) < v.(int) {
-				max = v
-			}
-		}
-	case string:
-		for _, v := range s {
-			if max.(string) < v.(string) {
-				max = v
-			}
-		}
+	for _, v := range s {
+		max = f(max, v)
 	}
 	return max
 }
 
 func main() {
 	s := []interface{}{1, 2, 3}
-	fmt.Println(findMax(s))
+
+	fmt.Println(findMax(s, func(max interface{}, v interface{}) interface{} {
+		if max.(int) < v.(int) {
+			max = v
+		}
+		return max
+	}))
 }
